343fef36e735dc9fe043dfaa4f1a2c9ad8dfebfb
Arul Evansyah <arul00.nc@gmail.com>
2026-09-02 21:45:46 +0800

    Day1.java
    Day 1 belajar coding

---
+++ Day1.java
@@ -0,0 +1,13 @@
+public class Day1 {
+    public static void main(String[] args) {
+
+        /* Hari pertama belajar Java */
+
+        /** Perjalanan 100 Days Coding dimulai */
+
+        // Komentar satu baris
+
+        System.out.println("Day 1 - Let's Start Coding!");
+
+    }
+}
