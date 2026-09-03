88a6036d64d1e5cae4e9ec0d128a9209a6f0202e
Arul Evansyah <arul00.nc@gmail.com>
2026-09-03 16:09:35 +0800

    Create Day2.java
    Day 2  : Membuat biodata diri dengan menggunakan println(), print() dan printf()

---
+++ Day2.java
@@ -0,0 +1,24 @@
+public class biodata {
+    public static void main(String[] args) {
+        // Biodata Diri //
+        String nama = "Arul Evansyah";
+        int umur = 19;
+        String alamat = "Kalukku";
+        double tinggi = 160.0;
+        
+
+        // Menggunakan println() //
+        System.out.println("==== Biodata Diri ====");
+
+        // Menggunakan print() //
+        System.out.print("Nama : ");
+        System.out.print("Arul Evansyah");
+        System.out.println();
+
+        // Menggunakan printf() //
+        System.out.printf("Umur : %d tahun%n", 19);
+        System.out.printf("Alamat : %s%n", "Kalukku");
+        System.out.printf("Tinggi : %d cm%n", 160);
+        
+    }
+}
