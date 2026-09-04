cd4a827e048d966a623fc4f25a58604ad47b3817
Arul Evansyah <arul00.nc@gmail.com>
2026-09-04 13:43:38 +0800

    Create Day3.java
    Day 3 : Penggunaan Escape Sequence (\n, \t dan lain-lain).

---
+++ Day3.java
@@ -0,0 +1,17 @@
+public class day3 {
+    public static void main(String[] args) {
+        // \n digunakan untuk pindah baris //
+        System.out.print("==== BIODATA DIRI ====\n");
+
+        // \t digunakan untuk memberikan jarak tab //
+        System.out.print("Nama\t:Arul Evansyah\n");
+        System.out.print("Umur\t:19\n");
+        System.out.print("Prodi\t:Informatika\n");
+
+        // \" digunakan untuk menampilkan tanda petik \" //
+        System.out.println("Motto\t:\"Terus Belajar\"");
+
+        // \\ digunakan untuk backslash //
+        System.out.println("Folder\t:C:\\BelajarJava");
+    }
+}
