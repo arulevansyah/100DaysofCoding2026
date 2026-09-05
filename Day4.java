5dc5b17dac74091af0b3c17e8da0b6554ce4e259
Arul Evansyah <arul00.nc@gmail.com>
2026-09-05 14:47:38 +0800

    Create Day4.java
    Day 4 : Tipe data numerik bilangan bulat  (byte, short, int, long)

---
+++ Day4.java
@@ -0,0 +1,21 @@
+public class day4 {
+    public static void main(String[] args) {
+        // Tipe data byte
+        byte umur = 19;
+
+        // Tipe data short
+        short tahunlahir = 2007;
+
+        // Tipe data int
+        int jumlahuang = 100000;
+
+        // Tipe data long
+        long jumlahpendudukdunia = 8000000000L;
+
+        // Menampilkan nilai variabel
+        System.out.println("Umur: " + umur);
+        System.out.println("Tahun Lahir: " + tahunlahir);
+        System.out.println("Jumlah Uang: " + jumlahuang);
+        System.out.println("Jumlah penduduk dunia: " + jumlahpendudukdunia);
+    }
+}
