8353f88525d51ab3480ebaba33b4ada0ed2dfc9d
Arul Evansyah <arul00.nc@gmail.com>
2026-09-06 18:20:28 +0800

    Day5.java
    Day 5 : Tipe data pecahan (double, float).

---
+++ Day5.java
@@ -0,0 +1,21 @@
+public class Day5 {
+    public static void main(String[] args) {
+        
+        /**
+         * Day 5 - Deklarasikan Tipe Data Pecahan
+         * Soal 1 : Mendeklarasikan berat dan tinggi.
+         */
+
+        // Deklarasi dan inisialisasi berat badan
+        double beratBadan = 55.5;
+
+        // Deklarasi dan inisialisasi tinggi badan
+        double tinggiBadan = 160.25;
+
+        // Mencetak berat badan menggunakan printf
+        System.out.printf("Berat badan: %.2f kg%n", beratBadan);
+
+        // Mencetak tinggi badan menggunakan printf
+        System.out.printf("Tinggi badan: %.2f cm%n", tinggiBadan);
+    }
+}
