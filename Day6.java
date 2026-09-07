b15aea37f910a96f2c766001ebe375af11a033d6
Arul Evansyah <arul00.nc@gmail.com>
2026-09-07 18:50:41 +0800

    Day6.java
    Day 6 : Tipe data karakter (char) dan boolean (boolean)

---
+++ Day6.java
@@ -0,0 +1,32 @@
+public class Day6 {
+    public static void main(String[] args) {
+
+        /**
+         * Day 6 - Tipe Data Karakter dan Boolean
+         * Soal: Membuat program sederhana untuk mendata mahasiswa.
+         */
+
+        // Nama menggunakan tipe data String
+        String nama = "Jefri";
+
+        // Umur menggunakan tipe data int
+        int umur = 20;
+
+        // IPK menggunakan tipe data double
+        double ipk = 3.75;
+
+        // Kode jurusan menggunakan tipe data char
+        char kodeJurusan = 'I';
+
+        // Status aktif kuliah menggunakan tipe data boolean
+        boolean status = true;
+
+        // Menampilkan data mahasiswa
+        System.out.println("Data Mahasiswa");
+        System.out.println("Nama\t\t: " + nama);
+        System.out.println("Umur\t\t: " + umur + " tahun");
+        System.out.println("IPK\t\t: " + ipk);
+        System.out.println("Kode Jurusan\t: " + kodeJurusan);
+        System.out.println("Status\t\t: " + status);
+    }
+}
