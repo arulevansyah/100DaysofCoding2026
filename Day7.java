2f55de1b35cf0ff3d2923bc75b4493331f045780
Arul Evansyah <arul00.nc@gmail.com>
2026-09-08 20:19:22 +0800

    Day7.java
    Day 7 : Pengenalan tipe data string untuk teks

---
+++ Day7.java
@@ -0,0 +1,10 @@
+public class Day7 {
+    public static void main(String[] args) {
+        
+        String nama = "Arul Evansyah";
+        String prodi = "Informatika";
+
+        System.out.println(nama);
+        System.out.println(prodi);
+    }
+}
