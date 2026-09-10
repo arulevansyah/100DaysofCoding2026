9df949f8c2e9f3025c3f34c19fa0087d75dda7fc
Arul Evansyah <arul00.nc@gmail.com>
2026-09-10 20:43:27 +0800

    Day9.java
    Day 9 : Constanta (final variabel)

---
+++ Day9.java
@@ -0,0 +1,18 @@
+public class day9 {
+    public static void main(String[] args) {
+        /**
+         * Day 9 - Constant (Final Variable)
+         */
+
+         // Constant menggunakan kayword final
+         final String NAMA = "Arul Evansyah";
+         final int ANGKATAN = 2026;
+         final String JURUSAN = "Informatika";
+
+         // Menampilkan data
+         System.out.println("=== DATA MAHASISWA ===");
+         System.out.println("Nama: " + NAMA);
+         System.out.println("Angkatan: " + ANGKATAN);
+         System.out.println("Jurusan: " + JURUSAN);
+    }
+}
