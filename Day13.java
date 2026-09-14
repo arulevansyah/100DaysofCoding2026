769a98c0b437e853da256430a8d76b60d89e9424
Arul Evansyah <arul00.nc@gmail.com>
2026-09-14 21:18:13 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,13 @@
+public class Day13 {
+    public static void main(String[] args) {
+
+        String nama = "Jefri";
+        String nim = "D0225009";
+        char kelas = 'A';
+        boolean mahasiswa = true;
+
+        System.out.println();
+        System.out.println("=== BIODATA MAHASISWA ===");
+        System.out.printf("Nama\t\t: %s%nNIM\t\t: %s%nkelas\t\t: %c%nMahasiswa\t: %b%n", nama, nim, kelas, mahasiswa);
+    }
+}
