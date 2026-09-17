f3b7db70088f121f002ba96ccea55b39fe3b9740
Arul Evansyah <arul00.nc@gmail.com>
2026-09-17 20:41:38 +0800

    Day16.java

---
+++ Day16.java
@@ -0,0 +1,13 @@
+public class Day16 {
+    public static void main(String[] args) {
+
+        int angka1 = 10;
+        int angka2 = 3;
+
+        int sisaBagi = angka1 % angka2;
+
+        System.out.println("Angka pertama: " + angka1);
+        System.out.println("Angka kedua: " + angka2);
+        System.out.println("Sisa bagi: " + sisaBagi);
+    }
+}
