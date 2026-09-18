13961c9fb6130db50feb817b1e49a16d10f82ee8
Arul Evansyah <arul00.nc@gmail.com>
2026-09-18 20:04:39 +0800

    Day17.java

---
+++ Day17.java
@@ -0,0 +1,30 @@
+public class Day17 {
+    public static void main(String[] args) {
+        
+        int angka = 10;
+
+        // Operator penugasan = 
+        angka = 20;
+        System.out.println("Hasil = : " + angka);
+
+        // Operator +=
+        angka += 5;
+        System.out.println("Hasil += : " + angka);
+
+        // Operator -=
+        angka -= 3;
+        System.out.println("Hasil -= : " + angka);
+
+        // Operator *=
+        angka *= 2;
+        System.out.println("Hasil *= : " + angka);
+
+        // Operator /=
+        angka /= 4;
+        System.out.println("Hasil /= : " + angka);
+        
+        // Operator %=
+        angka %= 3;
+        System.out.println("Hasil %= : " + angka);
+    }
+}
