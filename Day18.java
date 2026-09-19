a0c574b8d23db4a12ad3b61b1af4ac537f50eb7b
Arul Evansyah <arul00.nc@gmail.com>
2026-09-19 19:44:05 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,19 @@
+public class Day18 {
+    public static void main(String[] args) {
+        
+        byte nilaibyte = 100;
+        short nilaishort = nilaibyte;
+        int nilaiint = nilaishort;
+        long nilailong = nilaiint;
+        float nilaifloat = nilailong;
+        double nilaidouble = nilaifloat;
+
+        System.out.println("Nilai byte: " + nilaibyte);
+        System.out.println("Nilai short: " + nilaishort);
+        System.out.println("Nilai int: " + nilaiint);
+        System.out.println("Nilai long: " + nilailong);
+        System.out.println("Nilai float: " + nilaifloat);
+        System.out.println("Nilai double: " + nilaidouble);
+
+    }
+}
