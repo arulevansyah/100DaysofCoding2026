496bb2f88346531669f314b857d7b0140699bea6
Arul Evansyah <arul00.nc@gmail.com>
2026-09-21 16:28:08 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class Day20 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        // Input
+        System.out.print("Masukkan NIM: ");
+        int nim = s.nextInt();
+        System.out.print("Masukkan Umur: ");
+        int umur = s.nextInt();
+        System.out.print("Masukkan tinggi badan: ");
+        double tinggibadan = s.nextDouble();
+
+        // Konversi
+        String nilainim = String.valueOf(nim);
+        String nilaiumur = String.valueOf(umur);
+        String nilaitinggi = String.valueOf(tinggibadan);
+
+        // Penggabungan
+        String gabungan = nilaiumur + nilaitinggi;
+
+        // Output
+        System.out.println("=== DATA MAHASISWA ===");
+        System.out.println("NIM\t\t\t: " + nilainim);
+        System.out.println("Umur\t\t\t: " + nilaiumur);
+        System.out.println("Tinggi badan\t\t: " + nilaitinggi + " CM");
+        System.out.println("Gabungan Umur + Tinggi\t: " + gabungan);
+
+        s.close();
+    }
+    
+}
